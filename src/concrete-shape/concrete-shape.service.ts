34d047ab562037bf5534f9b9da56833d8092908d
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-01-24 19:19:30 -0500

    Modification of concrete service

--- src/concrete-shape/concrete-shape.service.ts
+++ src/concrete-shape/concrete-shape.service.ts
@@ -4,7 +4,6 @@ import { ShapeService } from '../shape/shape.service';
 @Injectable()
 export class ConcreteShapeService extends ShapeService {
   calculateArea(): number {
-    // Implement the calculation logic here
     return 0;
   }
 }
